de06fa6c66a18e9430c5c04b8b6187c8564eacf9
Yoru <m.f.fetvaci@hotmail.com>
2024-07-12 17:52:39 +0300

    Create emojis.rs

---
+++ src/utils/emojis.rs
@@ -0,0 +1,15 @@
+pub struct Grades;
+
+impl Grades {
+    pub const SSH: &'static str = "<:XH_:1057763296717045891>";
+    pub const XH: &'static str = "<:XH_:1057763296717045891>";
+    pub const SS: &'static str = "<:X_:1057763294707974215>";
+    pub const SH: &'static str = "<:SH_:1057763293491642568>";
+    pub const X: &'static str = "<:X_:1057763293491642568>";
+    pub const S: &'static str = "<:S_:1057763291998474283>";
+    pub const A: &'static str = "<:A_:1057763284327080036>";
+    pub const B: &'static str = "<:B_:1057763286097076405>";
+    pub const C: &'static str = "<:C_:1057763287565086790>";
+    pub const D: &'static str = "<:D_:1057763289121173554>";
+    pub const F: &'static str = "<:F_:1057763290484318360>";
+}
