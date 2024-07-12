45cf9a3ece6a0d0d73c277c0814c4ce1c8322b9e
Yoru <m.f.fetvaci@hotmail.com>
2024-07-13 01:31:53 +0300

    finish up profile command

--- src/commands/profile.rs
+++ src/commands/profile.rs
@@ -1,3 +1,4 @@
+use chrono::{Datelike, Utc};
 use num_format::{Locale, ToFormattedString};
 
 use serenity::builder::{CreateEmbed, CreateEmbedAuthor, CreateEmbedFooter, CreateMessage};
@@ -14,6 +15,9 @@ pub async fn execute(
     args: &Vec<&str>,
     handler: &Handler,
 ) -> Result<(), Error> {
+    // Start typing
+    msg.channel_id.start_typing(&ctx.http);
+
     let username = args.join(" ");
     if username.is_empty() {
         msg.channel_id
@@ -26,17 +30,18 @@ pub async fn execute(
         Ok(user) => {
             let statistics = user.statistics.expect("User statistics not found");
 
+            // Build author embed
             let author = CreateEmbedAuthor::new("")
                 .name(format!(
-                    "{}: {}pp (#{} {}#{})",
-                    user.username,
-                    statistics.pp,
-                    statistics
+                    "{username}: {pp}pp (#{global_rank} {country_code}#{country_rank})",
+                    username = user.username,
+                    pp = statistics.pp,
+                    global_rank = statistics
                         .global_rank
                         .unwrap_or(0)
                         .to_formatted_string(&Locale::en),
-                    user.country_code,
-                    statistics
+                    country_code = user.country_code,
+                    country_rank = statistics
                         .country_rank
                         .unwrap_or(0)
                         .to_formatted_string(&Locale::en)
@@ -45,7 +50,7 @@ pub async fn execute(
                     "https://osu.ppy.sh/images/flags/{}.png",
                     user.country_code
                 ))
-                .url(format!("https://osu.ppy.sh/users${}", user.user_id));
+                .url(format!("https://osu.ppy.sh/users/{}", user.user_id));
 
             let peak_rank_string = match user.highest_rank {
                 Some(peak_rank) => format!(
@@ -57,23 +62,58 @@ pub async fn execute(
             };
 
             let fields = vec![
-                ("Statistics :abacus:",
-                format!(
-                    "**Accuracy:**  `{:.2}` • **Level:** `{}.{:.2}%` \n**Playcount:** `{}` (`{} hrs`) \n**Peak Rank:** {}",
-                    statistics.accuracy, statistics.level.current, statistics.level.progress,
-                    statistics.playcount.to_formatted_string(&Locale::en), (statistics.playtime/3600).to_formatted_string(&Locale::en),
-                    peak_rank_string
+                (
+                    "Statistics :abacus:",
+                    format!(
+                        "**Accuracy:**  `{accuracy:.2}` • **Level:** `{level}.{progress:02}` \n\
+                        **Playcount:** `{playcount}` (`{playtime} hrs`) \n\
+                        **Peak Rank:** {peak_rank} \n\
+                        **Followers:** `{followers}` • **Max Combo:** `{max_combo}` \n\
+                        **Recommended Star Rating:** `{star_rating:.2}`★",
+                        accuracy = statistics.accuracy,
+                        level = statistics.level.current,
+                        progress = statistics.level.progress,
+                        playcount = statistics.playcount.to_formatted_string(&Locale::en),
+                        playtime = (statistics.playtime / 3600).to_formatted_string(&Locale::en),
+                        peak_rank = peak_rank_string,
+                        followers = user
+                            .follower_count
+                            .unwrap_or(0)
+                            .to_formatted_string(&Locale::en),
+                        max_combo = statistics.max_combo.to_formatted_string(&Locale::en),
+                        // Gets recommended star rating
+                        // https://www.reddit.com/r/osugame/comments/rj6pw4/comment/hp1jerz
+                        star_rating = statistics.pp.powf(0.4) * 0.195
+                    ),
+                    false,
                 ),
-                false,
+                (
+                    "Grades :mortar_board:",
+                    format!(
+                        "{}`{}` {}`{}` {}`{}` {}`{}` {}`{}`",
+                        Grades::SSH,
+                        statistics.grade_counts.ssh,
+                        Grades::SS,
+                        statistics.grade_counts.ss,
+                        Grades::SH,
+                        statistics.grade_counts.sh,
+                        Grades::S,
+                        statistics.grade_counts.s,
+                        Grades::A,
+                        statistics.grade_counts.a
+                    ),
+                    false,
                 ),
-                ("Grades :mortar_board:", format!("{}`{}` {}`{}` {}`{}` {}`{}` {}`{}`", Grades::SSH, statistics.grade_counts.ssh, Grades::SS, statistics.grade_counts.ss, Grades::SH, statistics.grade_counts.sh, Grades::S, statistics.grade_counts.s, Grades::A, statistics.grade_counts.a), false)
             ];
 
             let footer = CreateEmbedFooter::new(format!(
-                "Joined osu! on {}, {}, {}",
+                "Joined osu! on {}, {} {}, {}:{} ({} yrs ago)",
                 user.join_date.year(),
                 user.join_date.month(),
-                user.join_date.day()
+                user.join_date.day(),
+                user.join_date.hour(),
+                user.join_date.minute(),
+                Utc::now().year() - user.join_date.year()
             ));
 
             let embed = CreateEmbed::new()
