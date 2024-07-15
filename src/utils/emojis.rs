ed74732b83886a788e3ad09ecf2421be50b91f00
Yoru <m.f.fetvaci@hotmail.com>
2024-07-15 23:24:16 +0300

    rename osu! emojis in emojis.rs

--- src/utils/emojis.rs
+++ src/utils/emojis.rs
@@ -1,11 +1,9 @@
 pub struct Grades;
 
 impl Grades {
-    pub const SSH: &'static str = "<:XH_:1057763296717045891>";
     pub const XH: &'static str = "<:XH_:1057763296717045891>";
-    pub const SS: &'static str = "<:X_:1057763294707974215>";
+    pub const X: &'static str = "<:X_:1057763294707974215>";
     pub const SH: &'static str = "<:SH_:1057763293491642568>";
-    pub const X: &'static str = "<:X_:1057763293491642568>";
     pub const S: &'static str = "<:S_:1057763291998474283>";
     pub const A: &'static str = "<:A_:1057763284327080036>";
     pub const B: &'static str = "<:B_:1057763286097076405>";
